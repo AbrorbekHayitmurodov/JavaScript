f28c872abf32fdc97648325b004dc4761a481ee5
Abrorbek <khaitmuradovabrorbek@gmail.com>
2023-08-18 03:43:53 +0500

    Hammasi qo'shildi

---
+++ 1-dars/script.js
@@ -0,0 +1,70 @@
+// O'zgaruvchilar 
+// let, var, const
+// Data types = Ma'lumot turlari (Primitive- oddiy, Non-primitive- oddiy bo'lmagan)
+// PRIMITIVE 7 xil
+
+// 1-number
+// let a = 4;
+// console.log(a)
+// console.log (typeof a)
+
+// 2-bigint 
+// let a= 76497656476556467n;
+// console.log(typeof a)
+
+//3-string "", '', ``
+// let name1=`Webbrian Academy`;
+// console.log (name1);
+// console.log (typeof name1);
+
+//4-boolean = mantiqiy 
+// let a = 3, b=3;
+// console.log(a === b);
+
+//5-undefined 
+// let a;
+// console.log(a);
+
+//6-null
+// let a=null;
+// console.log(a);
+// console.log(typeof a);
+
+//7-symbol
+// let a=Symbol(1);
+// let b=Symbol(1);
+// console.log(a === b);
+
+//NON-PRIMITIVE (Object)
+// Object= object, array, function
+
+//1-object
+// let name = "Abror";
+// let study = "Webbrain Academy";
+// let age = 20;
+// let major = "student";
+// let user = {
+//     name:"Abror",
+//     study: "Webbrain Academy",
+//     age:20,
+//     major:"student"
+// }
+// console.log(user.major);
+
+//2-array
+// let name=["Xasan", "Xusan", "Jamshid"]
+// console.log(name[2]);
+
+//3-function= funksiya
+// function funksiya() {
+//     console.log ("Hello World");
+// }
+// let a=2;
+// funksiya();
+// funksiya();
+// funksiya();
+// alert ("Hello World")
+// let name="Alisa";
+// alert (`Hello ${"2"}`);
+// alert (`Hello ${"name"}`);
+// alert (`Hello ${name}`);
