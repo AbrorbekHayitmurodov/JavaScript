dd1d3fcffc9b50a3ef22098363bb759344d3a019
Abrorbek <khaitmuradovabrorbek@gmail.com>
2023-08-21 15:01:33 +0500

    Homework bajarildi

---
+++ 2-dars git & git hub/script.js
@@ -0,0 +1,6 @@
+// git init
+// git config --global user.name "Abror"
+// git config --global user.email "khaitmuradovabrorbek@gmail.com"
+// git remote add origin URL
+// git branch -m branchName
+// git commit -m "Comments"
---
+++ 3-dars type convertion/index.js
@@ -0,0 +1,66 @@
+// type convertion
+// string => number
+//  let a = "19";
+//  console.log(+a);
+//  console.log(typeof +a);
+// let a = "19.7af1929"
+// console.log(Number(a));
+// console.log(typeof Number(a));
+// console.log(parseInt(a));
+// console.log(parseFloat(a));
+// console.log(+a);
+// number => string 
+// let a=19;
+// console.log(String(a));
+// console.log(typeof String(a));
+// console.log(`${a}`);
+// ... => Boolean
+// String => Boolean
+// '', "",``
+// let a= "afsdgfhjkl"; //t
+// let b= " "; //t
+// let c= "266544"; //t
+// let d=""; //f
+// console.log(Boolean(a), Boolean(b), Boolean(c), Boolean(d));
+// Number => Boolean 
+// console.log(Boolean(0), Boolean(-19.7), Boolean(19.7));
+// console.log(Boolean(null), Boolean(undefined)); //f
+// console.log(Boolean({}), Boolean([])); //t
+// Boolean => ...
+// let a=true;
+// let b=false;
+// console.log(String(a));
+// console.log(String(b));
+// console.log(Number(a));
+// console.log(Number(b));
+
+// Operators
+// +, -, *, /, %, **, ++, --, =, ==, ===, +=
+// console.log(5 % 7);
+// Increment ++
+// let a=3;
+// let b="3";
+// let c=4;
+// a++;// a+1=a
+// console.log(a--); //3
+// console.log(--a); //1
+// console.log(a**3);
+// a/=2;
+// a=a+2;
+//Noqat'iy tenglik
+// console.log(a == b);
+//Qat'iy tenglik
+// console.log(a === b);
+
+// Comparisions => taqqoslash amallari
+// <, >, >=, <=
+// console.log(4 > 5);
+
+// Mantiqiy operatorlar
+// || , or , yoki, konyunksiya
+// console.log(false || null || null || undefined || "kljsga" );
+// && , va , and , dizyunksiya
+// console.log(2 && "jhsdkjas");
+
+// ! not , emas, inkor
+// console.log(!null);
