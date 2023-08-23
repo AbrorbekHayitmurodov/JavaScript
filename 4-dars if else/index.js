f548f104e3f07b46842f97ed3516c3291812d75c
Abrorbek <khaitmuradovabrorbek@gmail.com>
2023-08-23 13:12:36 +0500

    If else and Switch case

---
+++ 4-dars if else/index.js
@@ -0,0 +1,35 @@
+// if ("0") {
+//     console.log('Hello');
+// }  // Hello
+
+// let result;
+// if (a + b < 4) {
+//     result = 'Below';
+// } else {
+//     result = 'Over';
+// }
+// console.log(result);
+
+// let result;
+// (a + b < 4) ? result = 'Below' : result = 'Over';
+// console.log(result);
+
+// let message;
+// let login='Employee';
+// login == 'Employee' ? message = 'Hello'
+//     : login == 'Director' ? message = 'Greetings'
+//         : login == '' ? message = 'No login'
+//             : message = '';
+//             console.log(message);
+
+// let browser = "Firefox"
+// switch (browser) {
+//     case 'Edge': console.log("Yuo've got the Edge"); break;
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//         console.log('Okay we support these browsers too'); break;
+//     default:
+//         console.log('We hope that this page looks ok!'); break;
+// }
