1a351052fed4b074384d6f89835a05fa10dca476
Lenovo <khaitmuradovabrorbek@gmail.com>
2023-09-05 20:34:47 +0500

    Object Advenced

---
+++ 8-dars object 2/index.js
@@ -0,0 +1,22 @@
+// function makeUser(){
+//     return {
+//         name: "John",
+//         ref: this
+//     };
+// }
+
+// let user=makeUser();
+// console.log(user.ref.name);  // undefined
+
+// let str;
+// let a, b;
+// let calculator = {
+//     sum (a, b) {
+//         return console.log(a+b);
+//     },
+//     mult(a, b){
+//         return console.log(a*b);
+//     }
+// }
+// console.log(calculator.sum(2, 3));
+
