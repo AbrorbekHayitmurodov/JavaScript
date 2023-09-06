62e99cf108af100fad51466e41aecf200106dd13
Lenovo <khaitmuradovabrorbek@gmail.com>
2023-09-06 13:07:26 +0500

    Numbers qo'shildi

--- 9-dars numbers/index.js
+++ 9-dars numbers/index.js
@@ -63,3 +63,86 @@
 // myRound(2.4); // 2
 // myRound(-2.55); // -3
 // myRound(-2.4); // -2
+
+
+// Numbers
+
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+// Format
+// let n=1_000_000;
+// console.log(n);
+
+// Daraja
+// let n=1000;
+// let m=1e3;
+// console.log(n);
+// console.log(m);
+
+// parseInt // parseFloat
+
+// istalgan sanoq tizimi => 10  (2 => 36)
+// let n=17;
+// n=parseInt(n, 16);
+// console.log(n);
+
+// 10 => istalgan sanoq tizimiga o'tish
+// let n=12;
+// n=+n.toString(2);
+// console.log(n);
+
+// Yaxlitlash
+// toFixed, toPrecision
+// let n=124.56548;
+// n=n.toFixed(3);
+// n=n.toPrecision(3);
+// console.log(n);
+
+// Issue
+// console.log(0.3+0.5);
+// console.log(0.1+0.2);
+
+// Object is => ===
+// let n=12, m=12;
+// console.log(Object.is(n, m));
+
+// isNaN  => NaN likka tekshirish
+// console.log(isNaN(NaN)); // true
+// console.log(isNaN(13)); // false
+// console.log(isNaN(true)); // false
+// console.log(isNaN(null)); // false
+// console.log(isNaN(undefined)); // true
+// console.log(isNaN("webbrain")); // true
+// console.log(isNaN({ name: "webbrain" })); // true
+// console.log(isNaN(["webbrain"])); // true
+
+// Math => PI, E, LN2, log, sqrt, pow, abs, sin, cos, tan, 1/tan, floor, ceil, trunc, round, random [0; 1), min, max, sign
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.LN2);
+// console.log(Math.abs(-5));
+// console.log(Math.sqrt(16));
+// console.log(Math.cos(30));
+// console.log(Math.min(2,1,-3,-2));
+// console.log(Math.max(2,1,-3,-1,-3));
+// console.log(Math.sin(30));
+// console.log(Math.ceil(2.1));
+// console.log(Math.floor(2.9));
+// console.log(Math.trunc(2.8));
+// console.log(Math.round(2.55));
+// console.log(Math.round(2.45));
+// console.log(Math.sign(-55));
+// console.log(Math.sign(55));
+// console.log(Math.sign(0));
+// console.log(Math.sign(-0));
+
+// Random [0; 1)
+
+// console.log(Math.random());
+
+// [0; 20]
+// console.log(parseInt(Math.random()*21));
+
+// [10; 30]
+// console.log(parseInt(Math.random()*21+10));
